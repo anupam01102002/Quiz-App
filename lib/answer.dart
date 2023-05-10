95b22f896064632552b4e5744c15b14c2cae15b9
Anupam Anand <aanand20@iitk.ac.in>
2023-05-10 20:00:59 +0530

    Update

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selecthandler;
+  final String answerText;
 
-  Answer(this.selecthandler);
+  Answer(this.selecthandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      
       child: ElevatedButton(
         //color: Colors.blue,
-        child: Text('Answer 1'),
+        child: Text(answerText),
         onPressed: selecthandler,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 var _questionindex = 0;
+var _totalScore = 0;
 
 class MyApp extends StatefulWidget {
   @override
@@ -17,42 +18,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  void _answerQuestion() {
+  final questions = const [
+    {
+      'questionText': 'Baby frog is known as',
+      'answers': [
+        {'text': 'Tadpole', 'score': 10},
+        {'text': 'Tapddole', 'score': 0},
+        {'text': 'Tadpolo', 'score': 0},
+        {'text': 'Tadpol', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'How many consonants are there in the English alphabet?',
+      'answers': [
+        {'text': '18', 'score': 0},
+        {'text': '23', 'score': 0},
+        {'text': '21', 'score': 10},
+        {'text': '19', 'score': 0},
+      ],
+    },
+    {
+      'questionText': 'What is the National Anthem of India?',
+      'answers': [
+        {'text': 'Jana Gana Mana', 'score': 10},
+        {'text': '123', 'score': 0},
+        {'text': 'qwe', 'score': 0},
+        {'text': '456', 'score': 0},
+      ],
+    },
+  ];
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionindex = _questionindex + 1;
     });
 
     print(_questionindex);
+    if (_questionindex < questions.length) {
+      print('We have more Questons!');
+    } else {
+      print('No More Questions!');
+    }
   }
 
   Widget build(BuildContext context) {
-    var questions = [
-    {
-      'questionText': 'what\'s your favourite colour?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
-    },
-    {
-      'questionText': 'what\'s your favourite animal?',
-      'answers': ['Rabbit', 'Snake', 'Lion', 'Tiger'],
-    },
-    {
-      'questionText': 'what\'s your favourite instructor?',
-      'answers': ['Max', 'Min', 'Green', 'White'],
-    },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('Quiz App'),
         ),
-        body: Column(children: [
-          Question(
-            questions[_questionindex]['questionText'],
-          ),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-        ]),
+        body: _questionindex < questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionindex: _questionindex,
+                questions: questions,
+              )
+            : Result(_totalScore),
       ),
     );
   }
